3b6f95863cdbaa98e96f42b3b5ffd2ba8a91b346
Bodya567384 <itkaterynchuk124@gmail.com>
2024-02-03 14:46:03 +0200

    lesson1.2

--- p01/main.py
+++ p01/main.py
@@ -1,9 +1,13 @@
 # Тут знаходиться пояснення 
 
 print("Hello")
+
 print('Hello')
 # help(print)
 print('Hello','my','name','is','Bodya',124,527)
+
 print('Hello','my','name','is','Bodya',sep='\n')
+
 print('Hello'+'my'+'name'+'is'+'Bodya')
+
 print('Hello'+'my'+ '72' +'is'+'Bodya')
