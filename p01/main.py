3597cecf6383b348c2804512a98a8dc29e4b60e4
Bodya567384 <itkaterynchuk124@gmail.com>
2024-02-03 14:36:04 +0200

    lesson1

---
+++ p01/main.py
@@ -0,0 +1,9 @@
+# Тут знаходиться пояснення 
+
+print("Hello")
+print('Hello')
+# help(print)
+print('Hello','my','name','is','Bodya',124,527)
+print('Hello','my','name','is','Bodya',sep='\n')
+print('Hello'+'my'+'name'+'is'+'Bodya')
+print('Hello'+'my'+ '72' +'is'+'Bodya')
