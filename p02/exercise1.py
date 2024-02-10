d0066ae5e780ed4ad8d009717832ea589ba99b52
Bodya567384 <itkaterynchuk124@gmail.com>
2024-02-10 14:19:22 +0200

    lesson2

---
+++ p02/exercise1.py
@@ -0,0 +1,42 @@
+import sys
+import datetime as dt
+
+# print('Area of circle')
+# radius = int(input('Input radius: '))
+# pi = 3.1415
+# area = pi * int(radius) ** 2
+# print('Area of the circle = ', area)
+# 4 / 3 * pi * R ^ 3
+# print('Volume of sphere')
+# radius = int(input('Input radius: '))
+# pi = 3.1415
+# volume = 4 / 3 * pi * radius ** 3
+# print(f'Volume of the sphere {volume}')
+
+# computes the value of
+# n + nn + nnn
+# n = input('Input an integer')
+# n1 = n
+# n2 = n + n
+# n3 = n + n + n
+# sum = int(n1) + int(n2) + int(n3)
+# point(sum)
+# n = int(input('Input an integer: '))
+# n1 = int("%s" % n)
+# n2 = int("%s%s" % (n, n))
+# n = int("%s%s%s" % (n, n, n))
+# print(n1 + n2 + n3)
+
+# print("Python version")
+# print(sys.version)
+# print("Version info")
+# print(sys.version_info)
+
+# #Display the current date and time
+# now = dt.datetime.now()
+# print(now.strftime('%Y-%m-%d %H:%M:%S'))
+
+# UNICODE
+print('\x1b[0;32m\u2592\x1b[0;30m'*40)
+print('\u2592'*12, ' Unicode text ', '\u2592'*12)
+print('\x1b[5;32m\u2593\x1b[5;34m'*40)
