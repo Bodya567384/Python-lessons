e29d0b61be5567a3b4b0c1df4963093e13ccd896
Bodya567384 <itkaterynchuk124@gmail.com>
2024-02-24 14:14:50 +0200

    lesson06

---
+++ p06/exercise1.py
@@ -0,0 +1,45 @@
+import random
+# list1 = [5, 7, 8, 10, 1]
+# list2 = ['Ivan', 'Dima', 'Rita']
+# list3 = ['Ivan', 2, 17, 'Rita', True, 3.14]
+# list4 = [list1, list2, list3]
+# print(list4)
+# list1[0] = 999
+# print(list1[0])
+#
+# list_random_numbers = []
+# for i in range(10):
+#     list_random_numbers.append(random.randint(0, 10))
+# print(list_random_numbers)
+#
+# for item in list_random_numbers:
+#     print('Item = %d' % item)
+
+# for i in range(10):
+#     l = ['a', 'b', 'c', 'd', 'f']
+#     x = l[random.randint(0.4)]
+#     print(x, end='')
+#
+# nums = [1, 4, 6, 7, 4, 4, 7, 1, 2, 8]
+# print(nums)
+# count = 0
+# n = int(input('n: '))
+# for i in nums:
+#     if i == n:
+#         count = count + 1
+#         count += 1
+#
+# print('Count %d in NUMS = %d' % (n, count))
+# histogram = [12, 31, 6, 54, 9]
+# char = '\u2588'
+#
+# for a in histogram:
+#     print('')
+#     for n in range(a):
+#         print(char, end='')
+
+
+number = [3, 5, 12, 7, 8, 0, 22]
+
+for i in range(10, 20, 2):
+    print(i)
