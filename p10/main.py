2692392b21a9f771c456480282751b23eefd2fa1
Bodya567384 <itkaterynchuk124@gmail.com>
2024-03-16 14:28:14 +0200

    py-lessons 10

---
+++ p10/main.py
@@ -0,0 +1,25 @@
+# def crazy_boy_1():
+#     print('Text from crazy BOY')
+#
+# crazy_boy_1()
+#
+# def add_two_numbers(n1, n2):
+#     sum = n1 + n2
+#     if n1 == 0:
+#         return 0
+#
+#     print('Check')
+#
+#     return sum
+#
+# x = add_two_numbers(10, 5)
+# print(x)
+
+# def add_exaltation_square(number, up):
+#     result = number ** up
+#     return result
+#
+# print(add_exaltation_square(9, 3))
+# print(1)
+
+
